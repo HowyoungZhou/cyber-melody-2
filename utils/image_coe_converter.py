78654a8578ba6026b6f04afb5132a6ac506aba6d
Howyoung <howyoung3@gmail.com>
2020-08-13 15:17:08 +0800

    Add VRAM

--- utils/image_coe_converter.py
+++ utils/image_coe_converter.py
@@ -6,14 +6,17 @@ from coe_writer import write_coe
 
 def main():
     im = Image.open(argv[1])
-    write_coe(argv[2], 16, rgb12_pixel_generator(im))
+    pixels = list(rgb12_pixel_generator(im))
+    data = [(pixels[i] << 16) | pixels[i + 1] for i in range(0, len(pixels), 2)]
+    write_coe(argv[2], 16, data)
 
 
 def rgb12_pixel_generator(im):
+    print(f'Image size = {im.width} x {im.height}, bands = {im.getbands()}')
     for y in range(0, im.height):
         for x in range(0, im.width):
             pixel = im.getpixel((x, y))
-            rgb12_pixel = 0 # format = 0000rrrr ggggbbbb
+            rgb12_pixel = 0  # format = 0000rrrr ggggbbbb
             for i in range(0, 3):
                 rgb12_pixel = rgb12_pixel | math.floor(
                     pixel[i] / 255 * 15) << 4 * (2 - i)
