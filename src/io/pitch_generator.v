3ed3d4f5c295318f4c14cbec686c66ef0efd1604
Howyoung <howyoung3@gmail.com>
2020-08-13 11:39:12 +0800

    Add pitch generator

---
+++ src/io/pitch_generator.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:07:39 12/20/2019 
+// Design Name: 
+// Module Name:    pitch_generator 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module pitch_generator(
+    input [3:0] note,
+    input [3:0] octave,
+    input clk,
+    output wave
+    );
+
+    wire [7:0] period_addr = (note - 1) * 10 + octave;
+    wire [31:0] period;
+    wire en;
+
+    assign en = note != 0;
+
+    // just feed the period in the ROM to the wave generator
+    pitch_period_table pitch_period_table_rom (
+        .a(period_addr), // input [7 : 0] a
+        .spo(period) // output [31 : 0] spo
+    );
+
+    wave_generator wave_gen (
+        .clk(clk),
+        .en(en),
+        .period(period),
+        .wave(wave)
+    );
+endmodule
---
+++ src/io/wave_generator.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:26:55 12/20/2019 
+// Design Name: 
+// Module Name:    wave_generator 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module wave_generator(
+    input clk,
+    input en,
+    input [31:0] period,
+    output wave
+    );
+
+    parameter clk_period = 10;
+
+    reg [31:0] counter = 0;
+
+    assign wave = counter > period / 2;
+
+    always@(posedge clk)begin
+        if (en) begin
+            counter <= counter + clk_period;
+            if(counter >= period) counter <= 0;
+        end
+        else begin
+            counter <= 0;
+        end
+    end
+endmodule
